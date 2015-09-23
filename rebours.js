fdd10e90ea46b0b93ac534b15b6005de008db88c
Alban Nanty <banban@banban.org>
2015-09-23 18:17:47 +0800

    | make the sidebar, all the way down to the page, and not only a fixed value | position the date counter at the bottom of the page (well I guess the original design of the site intended to do that, but if not, we can also place the counter at the top of the sidebar) | fix the js script for the counter, for when the time overpass the competition date.

--- rebours.js
+++ rebours.js
@@ -1,42 +1,44 @@
 function compte_a_rebours()
 {
-	var compte_a_rebours = document.getElementById("compte_a_rebours");
+   var prefix = document.getElementById("compte_a_rebours_prefix");
+   var compte_a_rebours = document.getElementById("compte_a_rebours");
+   var legend = document.getElementById("compte_a_rebours_postfix")
 
-	var date_actuelle = new Date();
-	var date_evenement = new Date("Nov 30 00:00:00 2015");
-	var total_secondes = (date_evenement - date_actuelle) / 1000;
+   var date_actuelle = new Date();
+   var date_evenement = new Date("Nov 30 00:00:00 2015");
+   var competition_duration_in_sec = 86400; // Mat, je te laisse tuner la valeur, je sais pas combien de jours dure le concours...
+   var total_secondes = (date_evenement - date_actuelle) / 1000;
 
-	var prefixe = "Compte à rebours terminé dans ";
-	if (total_secondes < 0)
-	{
-		prefixe = "Compte à rebours terminé il y a "; // On modifie le préfixe si la différence est négatif
+   if (total_secondes < -competition_duration_in_sec)
+   {
+      prefix.innerHTML = "Le concours est terminé depuis"; // On modifie le préfixe si la différence est négatif
+      legend.innerHTML = "";
+      total_secondes = Math.abs(total_secondes); // On ne garde que la valeur absolue
+   }
 
-		total_secondes = Math.abs(total_secondes); // On ne garde que la valeur absolue
+   if (total_secondes > 0)
+   {
+      var jours = Math.floor(total_secondes / (60 * 60 * 24));
+      var mot_jour = "jours";
 
-	}
+      if (jours == 0)
+      {
+         jours = '';
+         mot_jour = '';
+      }
+      else if (jours == 1)
+      {
+         mot_jour = "jour";
+      }
 
-	if (total_secondes > 0)
-	{
-		var jours = Math.floor(total_secondes / (60 * 60 * 24));
-		var mot_jour = "jours";
-
-
-		if (jours == 0)
-		{
-			jours = '';
-			mot_jour = '';
-		}
-		else if (jours == 1)
-		{
-			mot_jour = "jour";
-		}
-
-		compte_a_rebours.innerHTML = jours + ' ' + mot_jour;
-	}
-	else
-	{
-		compte_a_rebours.innerHTML = 'Le concours est commencé.';
-	}
-	var actualisation = setTimeout("compte_a_rebours();", 100000);
+      compte_a_rebours.innerHTML = jours + ' ' + mot_jour;
+   }
+   else
+   {
+      prefix.innerHTML = "";
+      compte_a_rebours.innerHTML = 'Le concours est commencé.';
+      legend.innerHTML = "";
+   }
+   var actualisation = setTimeout("compte_a_rebours();", 100000);
 }
 compte_a_rebours();
