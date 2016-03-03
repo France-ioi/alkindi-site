8f788fa8572242d0fea4c4ba0b9eec541b35ab69
mathias-hiron <mathias.hiron@gmail.com>
2016-03-03 07:09:44 +0100

    mise à jour site pour tour 4

--- page.js
+++ page.js
@@ -5,41 +5,41 @@ myApp.config(function($routeProvider) {
 
    // route for the main page
    .when('/', {
-      templateUrl : 'pageMain.html?v=9',
+      templateUrl : 'pageMain.html?v=10',
       controller  : 'PageCtrl'
    })
 
    // route for the dicovery page
    .when('/pageDiscover', {
-      templateUrl : 'pageDiscover.html?v=9',
+      templateUrl : 'pageDiscover.html?v=10',
       controller  : 'PageCtrl'
    })
 
    // route for the manual page
    .when('/pageManual', {
-      templateUrl : 'pageManual.html?v=9',
+      templateUrl : 'pageManual.html?v=10',
       controller  : 'PageCtrl'
    })
 
    // route for the who are we page
    .when('/pageWhoAreWe', {
-      templateUrl : 'pageWhoAreWe.html?v=9',
+      templateUrl : 'pageWhoAreWe.html?v=10',
       controller  : 'PageCtrl'
    })
 
    // route for the register page
    .when('/pageRegister', {
-      templateUrl : 'pageRegister.html?v=9',
+      templateUrl : 'pageRegister.html?v=10',
       controller  : 'PageCtrl'
    })
    
    // route for the BD page
    .when('/pageBD', {
-      templateUrl : 'pageBD.html?v=9',
+      templateUrl : 'pageBD.html?v=10',
       controller  : 'PageCtrl'
    });
 });
 
 myApp.controller('PageCtrl', function() {
-    $("#monActu").load("actu.html?v=9"); 
+    $("#monActu").load("actu.html?v=10"); 
    });
