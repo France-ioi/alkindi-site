9032b9660e5672bcc1b379c1f0f83fbb47f55bef
mathias-hiron <mathias.hiron@gmail.com>
2016-03-06 22:31:52 +0100

    update rules (8 informations instead of 7)

--- page.js
+++ page.js
@@ -5,41 +5,41 @@ myApp.config(function($routeProvider) {
 
    // route for the main page
    .when('/', {
-      templateUrl : 'pageMain.html?v=10',
+      templateUrl : 'pageMain.html?v=11',
       controller  : 'PageCtrl'
    })
 
    // route for the dicovery page
    .when('/pageDiscover', {
-      templateUrl : 'pageDiscover.html?v=10',
+      templateUrl : 'pageDiscover.html?v=11',
       controller  : 'PageCtrl'
    })
 
    // route for the manual page
    .when('/pageManual', {
-      templateUrl : 'pageManual.html?v=10',
+      templateUrl : 'pageManual.html?v=11',
       controller  : 'PageCtrl'
    })
 
    // route for the who are we page
    .when('/pageWhoAreWe', {
-      templateUrl : 'pageWhoAreWe.html?v=10',
+      templateUrl : 'pageWhoAreWe.html?v=11',
       controller  : 'PageCtrl'
    })
 
    // route for the register page
    .when('/pageRegister', {
-      templateUrl : 'pageRegister.html?v=10',
+      templateUrl : 'pageRegister.html?v=11',
       controller  : 'PageCtrl'
    })
    
    // route for the BD page
    .when('/pageBD', {
-      templateUrl : 'pageBD.html?v=10',
+      templateUrl : 'pageBD.html?v=11',
       controller  : 'PageCtrl'
    });
 });
 
 myApp.controller('PageCtrl', function() {
-    $("#monActu").load("actu.html?v=10"); 
+    $("#monActu").load("actu.html?v=11"); 
    });
