923a2071de56e97bdc17b2b718c4418473a3b20e
mathias-hiron <mathias.hiron@gmail.com>
2016-04-20 07:58:00 +0200

    suite et fin BD

--- page.js
+++ page.js
@@ -5,41 +5,41 @@ myApp.config(function($routeProvider) {
 
    // route for the main page
    .when('/', {
-      templateUrl : 'pageMain.html?v=11',
+      templateUrl : 'pageMain.html?v=15',
       controller  : 'PageCtrl'
    })
 
    // route for the dicovery page
    .when('/pageDiscover', {
-      templateUrl : 'pageDiscover.html?v=11',
+      templateUrl : 'pageDiscover.html?v=15',
       controller  : 'PageCtrl'
    })
 
    // route for the manual page
    .when('/pageManual', {
-      templateUrl : 'pageManual.html?v=11',
+      templateUrl : 'pageManual.html?v=15',
       controller  : 'PageCtrl'
    })
 
    // route for the who are we page
    .when('/pageWhoAreWe', {
-      templateUrl : 'pageWhoAreWe.html?v=11',
+      templateUrl : 'pageWhoAreWe.html?v=15',
       controller  : 'PageCtrl'
    })
 
    // route for the register page
    .when('/pageRegister', {
-      templateUrl : 'pageRegister.html?v=11',
+      templateUrl : 'pageRegister.html?v=15',
       controller  : 'PageCtrl'
    })
    
    // route for the BD page
    .when('/pageBD', {
-      templateUrl : 'pageBD.html?v=11',
+      templateUrl : 'pageBD.html?v=15',
       controller  : 'PageCtrl'
    });
 });
 
 myApp.controller('PageCtrl', function() {
-    $("#monActu").load("actu.html?v=11"); 
+    $("#monActu").load("actu.html?v=15"); 
    });
