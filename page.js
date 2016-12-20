c20ae993a7b2fa216b717251941835888ea72b75
mathias-hiron <mathias.hiron@gmail.com>
2016-12-20 23:37:39 +0100

    maj actus

--- page.js
+++ page.js
@@ -5,19 +5,19 @@ myApp.config(function($routeProvider) {
 
    // route for the main page
    .when('/', {
-      templateUrl : 'pageMain.html?v=16',
+      templateUrl : 'pageMain.html?v=18',
       controller  : 'PageCtrl'
    })
 
    // route for the dicovery page
    .when('/pageDiscover', {
-      templateUrl : 'pageDiscover.html?v=16',
+      templateUrl : 'pageDiscover.html?v=18',
       controller  : 'PageCtrl'
    })
 
    // route for the manual page
    .when('/pageManual', {
-      templateUrl : 'pageManual.html?v=16',
+      templateUrl : 'pageManual.html?v=18',
       controller  : 'PageCtrl'
    })
 
@@ -29,34 +29,34 @@ myApp.config(function($routeProvider) {
     
    // route for the previous page
    .when('/pagePrevious', {
-      templateUrl : 'pagePrevious.html?v=16',
+      templateUrl : 'pagePrevious.html?v=18',
       controller  : 'PageCtrl'
    })
    // route for the who are we page
    .when('/pageWhoAreWe', {
-      templateUrl : 'pageWhoAreWe.html?v=16',
+      templateUrl : 'pageWhoAreWe.html?v=18',
       controller  : 'PageCtrl'
    })
 
    // route for the register page
    .when('/pageInfosLegales', {
-      templateUrl : 'pageInfosLegales.html?v=16',
+      templateUrl : 'pageInfosLegales.html?v=18',
       controller  : 'PageCtrl'
    })
    
    // route for the BD page
    .when('/pageBD', {
-      templateUrl : 'pageBD.html?v=16',
+      templateUrl : 'pageBD.html?v=18',
       controller  : 'PageCtrl'
    })
 
    // route for the Visits page
    .when('/pageVisits', {
-      templateUrl : 'pageVisits.html?v=16',
+      templateUrl : 'pageVisits.html?v=18',
       controller  : 'PageCtrl'
    });
 });
 
 myApp.controller('PageCtrl', function() {
-    $("#monActu").load("actu.html?v=16"); 
+    $("#monActu").load("actu.html?v=18"); 
    });
