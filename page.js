d522d67d81345d69a16dfb602acef01851b5bafa
Melanie Storup <m.storup@epixode.fr>
2018-10-18 23:32:28 +0200

    change videos layout, use ng-repeat to create videos lists

--- page.js
+++ page.js
@@ -95,11 +95,67 @@ myApp.controller('activeMenuItem', function($scope, $location){
    };
 });
 
-myApp.controller('VideoDisplay', function($scope, $sce) {
-   $scope.displayPlayer = false;
-   $scope.loadVideo = function (item) {
-      $scope.videoUrl = $sce.trustAsResourceUrl(item.currentTarget.getAttribute('data-source'));
-      $scope.displayPlayer = true;
-   };
-
+myApp.controller('VideosLists', function($scope) {
+   $scope.videosPresentation = [
+      {
+         title: 'Lancement de la 3e édition du concours Alkindi',
+         source: 'https://www.youtube.com/embed/f_y6mJPvlIA',
+         thumbnail: 'images/vignetteVideo_lancement.jpg'
+      },
+      {
+         title: 'Témoignages de participants au 1er tour du concours 2017-2018',
+         source: 'https://www.youtube.com/embed/ioxT4H7Kx6M',
+         thumbnail: 'images/vignetteVideo_participants.jpg'
+      },
+      {
+         title: 'Interview de Gérard Berry, parrain du concours Alkindi',
+         source: 'https://www.youtube.com/embed/4i-4JlKPC6I',
+         thumbnail: 'images/vignetteVideo_gerardBerry.jpg'
+      },
+      {
+         title: 'Visite d\'un laboratoire de cryptographie par des équipes du concours',
+         source: 'https://www.youtube.com/embed/EVLHEOWAORc',
+         thumbnail: 'images/vignetteVideo_visites.jpg'
+      },
+      {
+         title: 'Lê, auteur de la chaîne Youtube Science4all, a rencontré les lauréats d\'une académie',
+         source: 'https://www.youtube.com/embed/CZbTjvSRZhM',
+         thumbnail: 'images/vignetteVideo_science4all.jpg'
+      }
+   ];
+   $scope.videosPreviousContests = [
+      {
+         title: 'Exercice "Réseau" corrigé par Matthieu Lequesne',
+         source: 'https://www.youtube.com/embed/Mv415zfUFNs',
+         thumbnail: 'images/Miniature-vidéo-Matthieu.JPG'
+      },
+      {
+         title: 'Exercice "Connexions" corrigé par Anne Canteaut',
+         source: 'https://www.youtube.com/embed/8ohEeTPKBwA',
+         thumbnail: 'images/Miniature-vidéo-Anne-Canteaut.JPG'
+      },
+      {
+         title: 'Exercice "Aliens" corrigé par Razvan Barbulescu',
+         source: 'https://www.youtube.com/embed/8DQtOH1WwBs',
+         thumbnail: 'images/Miniature-vidéo-Razvan.JPG'
+      },
+      {
+         title: 'Exercice "Générateur" corrigé par Christina Boura',
+         source: 'https://www.youtube.com/embed/Y-VQBzwEaqQ',
+         thumbnail: 'images/Miniature-vidéo-Christina.JPG'
+      },
+      {
+         title: 'Exercice "Coffre fort" corrigé par Aline Gouget',
+         source: 'https://www.youtube.com/embed/qturJa6f97M',
+         thumbnail: 'images/Miniature-vidéo-Aline-Gouget.JPG'
+      }
+   ];
+   $scope.setActiveItem = function(item) {
+      $scope.activeItem = item;
+   }
 });
+myApp.filter('trustAsResourceUrl', ['$sce', function($sce) {
+    return function(val) {
+        return $sce.trustAsResourceUrl(val);
+    };
+}])
